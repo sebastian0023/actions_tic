8eb67b3677f7c6347d566bbddadaec674a918797
sebastian0023 <130592260+sebastian0023@users.noreply.github.com>
2026-04-27 13:24:23 -0600

    git-work

---
+++ app.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+let i = 5;
+let j = 10;
+console.log("inicio");
+console.log("la suma es " + (5 + 10));
+
+let fecha = moment().format("LL");
+console.log("la fecha es " + fecha);
+console.log("fin");
